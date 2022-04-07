944d386b18a8ffc408183459212ad4e621a8f86d
Franzi Graml <franzi.graml@gmx.de>
2022-04-07 14:16:09 +0200

    JS-Card

--- Components/Cards/cards.js
+++ Components/Cards/cards.js
@@ -4,18 +4,17 @@ const answerText = document.querySelector('[data-js="hidden-text"]');
  */
 
 export default function Card(cardElement) {
-    const button = cardElement.querySelector('[data-js="button-show"]');
-    const buttonHide = cardElement.querySelector('[data-js="button-hide"]');
-    const answerText = cardElement.querySelector('[data-js="hidden-text"]');
-    button.addEventListener('click', () => {
-    answerText.classList.toggle('hidden');
-    buttonHide.classList.toggle('hidden');
-    button.classList.toggle('hidden');
-    });
-    buttonHide.addEventListener('click', () => {
-    answerText.classList.toggle('hidden');
-    buttonHide.classList.toggle('hidden');
-    button.classList.toggle('hidden');
-    });   
-
+  const button = cardElement.querySelector('[data-js="button-show"]');
+  const buttonHide = cardElement.querySelector('[data-js="button-hide"]');
+  const answerText = cardElement.querySelector('[data-js="hidden-text"]');
+  button.addEventListener("click", () => {
+    answerText.classList.toggle("hidden");
+    buttonHide.classList.toggle("hidden");
+    button.classList.toggle("hidden");
+  });
+  buttonHide.addEventListener("click", () => {
+    answerText.classList.toggle("hidden");
+    buttonHide.classList.toggle("hidden");
+    button.classList.toggle("hidden");
+  });
 }
